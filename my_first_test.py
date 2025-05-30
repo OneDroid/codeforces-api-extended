3b82a2a4b52aa85db471c0db7f4c3c4ed6bd750e
tawhidmonowar <tawhidmonowar@gmail.com>
2025-05-30 17:04:08 +0600

    update workflow

--- my_first_test.py
+++ my_first_test.py
@@ -3,11 +3,10 @@ from seleniumbase import SB
 
 results = []
 results.append({
-    "text": "Hello World",
+    "text": "Hello World222",
 })
 
 # Write the extracted data to a JSON file
 with open("data.json", "w", encoding="utf-8") as f:
     json.dump(results, f, indent=2, ensure_ascii=False)
-
 
